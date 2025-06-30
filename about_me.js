f7a6bac9f458115ac422b54c0b3b853afe79f9d2
AlexMak-87 <makedonskij100@gmail.com>
2025-06-30 15:41:09 +0400

    30/06

---
+++ about_me.js
@@ -0,0 +1,10 @@
+// Информация обо мне
+let name = "Александр";
+let age = 38;
+let city = "Тбилиси";
+let hobby = "Путешествия и веб-разработка";
+
+console.log("Имя:", name);
+console.log("Возраст:", age);
+console.log("Город:", city);
+console.log("Хобби:", hobby);

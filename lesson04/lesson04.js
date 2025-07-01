e3b5de0b77ef78ca28acc07d70ef9ba0ab1c250c
AlexMak-87 <makedonskij100@gmail.com>
2025-07-01 17:08:53 +0400

    01,07

---
+++ lesson04/lesson04.js
@@ -0,0 +1,22 @@
+// let x = 11;
+
+// if(x > 10) {
+//     console.log('Условие верно')
+// } else {
+//     console.log('Условие не верно')
+// }
+
+
+
+let a =5
+let b ='10'
+let res = a + +b
+
+console.log(a.toString());
+console.log(res);
+console.log(typeof a);
+console.log(typeof b);
+
+console.log(a+b);
+
+alert('Заражение компьютера')

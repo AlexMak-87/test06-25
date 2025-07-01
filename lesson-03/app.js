b4ccba34ddf43eb609d9f792315ec48f49eda4fc
AlexMak-87 <makedonskij100@gmail.com>
2025-07-01 16:00:14 +0400

    01.07

---
+++ lesson-03/app.js
@@ -0,0 +1,13 @@
+let titleProject = "Оценка стоимости";
+let screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+let screenPrice = 5000; // примерная цена за один экран
+let percentage = 30; // процент отката подрядчику
+let fullPrice = 80000; // желаемый заработок с проекта
+let responsive = true; // адаптивность проекта
+
+console.log("Название проекта:", titleProject);
+console.log("Типы экранов:", screensValue);
+console.log("Цена за экран:", screenPrice);
+console.log("Процент отката:", percentage + "%");
+console.log("Желаемый доход:", fullPrice);
+console.log("Адаптивность нужна:", responsive);
